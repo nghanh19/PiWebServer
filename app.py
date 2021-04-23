e2b6fa4b0cc3c8eec00d76b9775cbde512f5ad37
nghanh19 <nghanh0993@outlook.com>
2021-04-23 10:09:40 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import  json
+import  requests
+
+from flask      import Flask, jsonify, request, render_template
+from argparse   import ArgumentParser
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cakes')
+def cakes():
+    return render_template('cakes.html')  
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(debug=True, host='0.0.0.0', port=port)
